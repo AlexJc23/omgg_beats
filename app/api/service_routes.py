ad3422c7c8993db7e1283155e30877c47d192a4e
Alex Carl <alexjcarl97@gmail.com>
2025-06-01 22:34:38 -0400

    endpoint for creating a new service

--- app/api/service_routes.py
+++ app/api/service_routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, jsonify, request
 from app import db
-from app.models import Service, User
+from app.models import Service, User, ServiceImage
 from flask_login import login_required, current_user
+from app.forms import ServiceForm
+from app.api.s3_helper import upload_file_tos3, get_unique_filename
 
 service_routes = Blueprint('services', __name__)
 
@@ -28,3 +30,53 @@ def get_service(service_id):
     return jsonify(service.to_dict()), 200
 
 
+@service_routes.route('/', methods=['POST'])
+@login_required
+def create_service():
+    """
+    Create a new service
+    """
+    form = ServiceForm()
+    form['csrf_token'].data = request.cookies.get('csrf_token')
+    print("Form data:", form.data)
+    if form.validate_on_submit():
+        try:
+            service = Service(
+                name=form.data['name'],
+                description=form.data['description'],
+                price=form.data['price'],
+                details=form.data['details']
+            )
+            db.session.add(service)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({"error": "Failed to create service", "details": str(e)}), 500
+
+        # Handle image uploads from request.files (not form.data)
+        if form.data['image']:
+            image = form.data['image']
+            image.filename = get_unique_filename(image.filename)
+            upload_response = upload_file_tos3(image)
+            print(upload_response)
+
+            if "url" not in upload_response:
+                    db.session.rollback()
+                    return jsonify({"error": "Failed to upload image", "details": upload_response.get("errors", "Unknown error")}), 500
+
+            service_image = ServiceImage(
+                service_id=service.id,
+                s3_url=upload_response['url']
+            )
+
+            db.session.add(service_image)
+            db.session.commit()
+        else:
+            service_image = ServiceImage(
+                service_id=service.id,
+                s3_url=None
+            )
+            db.session.add(service_image)
+            db.session.commit()
+        return jsonify(service.to_dict()), 201
+    return jsonify(form.errors), 400
--- app/forms/service_form.py
+++ app/forms/service_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField, FileField
-from flask_wtf.file import FileAllowed
+from wtforms import StringField, IntegerField, TextAreaField, MultipleFileField, FileField
+from flask_wtf.file import FileAllowed, FileRequired
 from wtforms.validators import DataRequired, Length, NumberRange
 
 
@@ -9,5 +9,6 @@ class ServiceForm(FlaskForm):
     description = TextAreaField('description', validators=[DataRequired(), Length(max=1000)])
     price = IntegerField('price', validators=[DataRequired(), NumberRange(min=0)])
     details = TextAreaField('details', validators=[DataRequired(), Length(max=2000)])
-    image = FileField('Images', validators=[
-        FileAllowed(['jpg', 'jpeg', 'png', 'gif'], 'Images only!')])
+    image = FileField('Image File', validators=[ FileRequired(),
+        FileAllowed(['jpg', 'jpeg', 'png', 'gif'])
+    ])
